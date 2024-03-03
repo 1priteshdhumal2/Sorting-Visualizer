1c32ee884cd554630829bae9e7a00114c362e519
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-03-03 21:00:08 +0530

    Implemented quick sort

--- sorting.js
+++ sorting.js
@@ -13,8 +13,7 @@ function disableSortingBtn() {
     document.querySelector(".bubbleSort").disabled = true;
     document.querySelector(".insertionSort").disabled = true;
     document.querySelector(".mergeSort").disabled = true;
-    document.querySelector(".quickSort1").disabled = true;
-    document.querySelector(".quickSort2").disabled = true;
+    document.querySelector(".quickSort").disabled = true;
     document.querySelector(".selectionSort").disabled = true;
     document.querySelector(".heapSort").disabled = true;
 }
@@ -24,8 +23,7 @@ function enableSortingBtn() {
     document.querySelector(".bubbleSort").disabled = false;
     document.querySelector(".insertionSort").disabled = false;
     document.querySelector(".mergeSort").disabled = false;
-    document.querySelector(".quickSort1").disabled = false;
-    document.querySelector(".quickSort2").disabled = false;
+    document.querySelector(".quickSort").disabled = false;
     document.querySelector(".selectionSort").disabled = false;
     document.querySelector(".heapSort").disabled = false;
 }
